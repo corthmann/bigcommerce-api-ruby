305f35d56032dcb9c1839c9584afc9dde0b821d2
Christian Orthmann <christian.orthmann@hotmail.com>
2014-09-30 21:30:02 +0200

    fix api configuration with ssl

--- lib/bigcommerce/connection.rb
+++ lib/bigcommerce/connection.rb
@@ -27,19 +27,19 @@ module Bigcommerce
     end
 
     def ssl_ca_file=(path)
-      @configuration.ssl_ca_file = path
+      @configuration[:ssl_ca_file] = path
     end
 
     def ssl_client_key=(path,passphrase=nil)
       if passphrase.nil?
-        @configuration.ssl_client_key = OpenSSL::PKey::RSA.new(File.read(path))
+        @configuration[:ssl_client_key] = OpenSSL::PKey::RSA.new(File.read(path))
       else
-        @configuration.ssl_client_key = OpenSSL::PKey::RSA.new(File.read(path), passphrase)
+        @configuration[:ssl_client_key] = OpenSSL::PKey::RSA.new(File.read(path), passphrase)
       end
     end
 
     def ssl_client_cert=(path)
-      @configuration.client_cert = OpenSSL::X509::Certificate.new(File.read(path))
+      @configuration[:client_cert] = OpenSSL::X509::Certificate.new(File.read(path))
     end
 
     def get(path, options = {}, headers = {})
